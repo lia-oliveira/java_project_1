83b3f37a4f4556b517a6a51d5c30575e02164c83
lia-oliveira <dev.gillian@gmail.com>
2025-03-20 21:40:56 -0300

    Payment, one-to-one association

--- src/main/java/com/oliveiralia/project1/config/TestConfig.java
+++ src/main/java/com/oliveiralia/project1/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.oliveiralia.project1.entities.Category;
 import com.oliveiralia.project1.entities.Order;
 import com.oliveiralia.project1.entities.OrderItem;
+import com.oliveiralia.project1.entities.Payment;
 import com.oliveiralia.project1.entities.Product;
 import com.oliveiralia.project1.entities.User;
 import com.oliveiralia.project1.entities.enums.OrderStatus;
@@ -38,6 +39,8 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private OrderItemRepository orderItemRepository;
+	
+	
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -82,6 +85,9 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"),o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 		
 	}
 
